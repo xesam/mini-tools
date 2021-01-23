7161f048fd99f37a145e8fcff92032d263720a5a
xesam <xesamguo@gmail.com>
2021-01-23 18:41:22 +0800

    encode fix

--- pages/encode/index.js
+++ pages/encode/index.js
@@ -2,24 +2,75 @@ import {_Page as Page} from '../../miniapp/miniapp';
 
 Page({
     data: {
-        content: ''
+        functions: [
+            encodeURIComponent.name,
+            decodeURIComponent.name,
+            encodeURI.name,
+            decodeURI.name,
+            'base64 encode',
+            'base64 decode'
+        ],
+        currentFunc: encodeURIComponent.name,
+        input: '',
+        output: '',
     },
     onLoad(query) {
 
     },
-    onTapCopy(e) {
-        wx.setClipboardData({
-            data: this.data.content
+    getFunction(name) {
+        if (name === encodeURIComponent.name) {
+            return encodeURIComponent;
+        } else if (name === decodeURIComponent.name) {
+            return decodeURIComponent;
+        } else if (name === encodeURI.name) {
+            return encodeURI;
+        } else if (name === decodeURI.name) {
+            return decodeURI;
+        }
+    },
+    toOutput(value) {
+        const func = this.getFunction(this.data.currentFunc);
+        return func(value);
+    },
+    onInputChanged(e) {
+        const value = e.detail.value;
+        const output = this.toOutput(value);
+        this.setData({
+            input: value,
+            output
         });
     },
-    onTapQr(e) {
-        wx.scanCode({
-            success: res => {
-                console.log(res);
-                this.setData({
-                    content: res.result
-                });
-            }
-        })
+    onOutputChanged(e) {
+        const value = e.detail.value;
+        this.setData({
+            output: value
+        });
+    },
+    onTapCopy(e) {
+        const {source} = e.target.dataset;
+        wx.setClipboardData({
+            data: this.data[source]
+        });
+    },
+    onTapClear(e) {
+        const {source} = e.target.dataset;
+        if (source === 'input') {
+            this.setData({
+                input: '',
+                output: ''
+            });
+        } else {
+            this.setData({
+                output: ''
+            });
+        }
+    },
+    onTapExchange(e) {
+        const newInput = this.data.output;
+        const newOutput = this.toOutput(newInput);
+        this.setData({
+            input: newInput,
+            output: newOutput
+        });
     }
 });
