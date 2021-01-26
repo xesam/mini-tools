b583ccc988315d88c44e315cbc35800da22b222a
xesam <xesamguo@gmail.com>
2021-01-26 10:11:23 +0800

    func fix

--- pages/loc/index.js
+++ pages/loc/index.js
@@ -2,6 +2,7 @@ import {_Page as Page} from '../../miniapp/miniapp';
 
 Page({
     data: {
+        loading: false,
         date: null,
         myWgsLocation: null,
         myGcjLocation: null,
@@ -12,6 +13,9 @@ Page({
     },
     refresh() {
         const _this = this;
+        _this.setData({
+            loading: true
+        });
         wx.getLocation({
             type: 'wgs84',
             success(res) {
@@ -22,15 +26,27 @@ Page({
                 wx.getLocation({
                     type: 'gcj02',
                     success(res) {
+                        const datetime = new Date();
                         _this.setData({
-                            date: new Date().toLocaleString(),
+                            loading: false,
+                            date: `${datetime.getFullYear()}-${datetime.getMonth() + 1}-${datetime.getDate()} ${datetime.getHours()}:${datetime.getMinutes()}:${datetime.getSeconds()}`,
                             myWgsLocation: wgs,
                             myGcjLocation: {
                                 latitude: res.latitude,
                                 longitude: res.longitude
                             }
                         });
+                    },
+                    fail() {
+                        _this.setData({
+                            loading: false
+                        });
                     }
+                });
+            },
+            fail() {
+                _this.setData({
+                    loading: false
                 });
             }
         });
@@ -53,5 +69,25 @@ Page({
                 )
             }
         })
+    },
+    location2String(location) {
+        return `${location.longitude},${location.latitude}`;
+    },
+    onTapCopy(e) {
+        const target = e.currentTarget.dataset.target;
+        console.log(target);
+        let copyData = null;
+        if (target === 'my-wgs') {
+            copyData = this.location2String(this.data.myWgsLocation);
+        } else if (target === 'my-gcj') {
+            copyData = this.location2String(this.data.myGcjLocation);
+        } else if (target === 'search-address') {
+            copyData = this.data.searchLocation.address;
+        } else if (target === 'search-location') {
+            copyData = this.location2String(this.data.searchLocation);
+        }
+        wx.setClipboardData({
+            data: copyData
+        });
     }
 });
