43d3005aed98ffb92f023f73a90d543ff15ffe80
xesam <xesamguo@gmail.com>
2021-01-29 17:10:49 +0800

    style fix

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import {_App as App} from './miniapp/miniapp';
 import appRouter from "./app.router";
 
 App({
+    $VERSION: '0.0.2',
     $router: appRouter,
     onLaunch(options) {
 
---
+++ pages/me/index.js
@@ -0,0 +1,11 @@
+import {_Page as Page} from '../../miniapp/miniapp';
+
+const app = getApp();
+Page({
+    data: {
+        version: app.$VERSION
+    },
+    onLoad(query) {
+
+    }
+});
