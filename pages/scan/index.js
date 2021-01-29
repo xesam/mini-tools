cb12be4cc8f11bd6d92b809128ef282d09cb9c83
xesam <xesamguo@gmail.com>
2021-01-29 15:02:53 +0800

    scan fix

--- pages/scan/index.js
+++ pages/scan/index.js
@@ -2,22 +2,28 @@ import {_Page as Page} from '../../miniapp/miniapp';
 
 Page({
     data: {
-        content: ''
+        result: '',
+        scanType: '',
+        charSet: '',
+        path: '',
+        rawData: ''
     },
     onLoad(query) {
 
     },
     onTapCopy(e) {
         wx.setClipboardData({
-            data: this.data.content
+            data: this.data.result
         });
     },
     onTapQr(e) {
         wx.scanCode({
+            // scanType: ['barCode', 'qrCode', 'datamatrix', 'pdf417'], //真机表现有问题？？？？
             success: res => {
                 console.log(res);
                 this.setData({
-                    content: res.result
+                    ...res,
+                    path: res.path
                 });
             }
         })
