2665aa684657def20add3245caa8acb7f65858e9
xesam <xesamguo@gmail.com>
2021-02-03 19:11:17 +0800

    color new

--- pages/color/index.js
+++ pages/color/index.js
@@ -1,15 +1,46 @@
 import {_Page as Page} from '../../miniapp/miniapp';
 
+const INVALID = '-1';
+
 Page({
     data: {
-        hex: ''
+        bgColor: 'white',
+        fullHex: '-1'
     },
     onLoad(query) {
     },
+    onTapCopy(e) {
+        wx.setClipboardData({
+            data: this.data.fullHex
+        });
+    },
+    onBgChanged(e) {
+        const value = e.detail.value;
+        this.setData({
+            bgColor: value
+        });
+    },
     onHexChanged(e) {
         const hex = e.detail.value;
-        this.setData({
-            hex: hex
-        });
+        if (/^[\d|a-f]+$/i.test(hex)) {
+            let fullHex = INVALID;
+            if (hex.length === 3 || hex.length === 4) {
+                fullHex = hex.split('').map(ele => {
+                    return ele + ele;
+                }).join('');
+            } else if (hex.length === 6 || hex.length === 8) {
+                fullHex = hex;
+            }
+            if (fullHex.length === 6) {
+                fullHex += 'ff';
+            }
+            this.setData({
+                fullHex: `#${fullHex.toUpperCase()}`
+            });
+        } else {
+            this.setData({
+                fullHex: INVALID
+            });
+        }
     }
 });
