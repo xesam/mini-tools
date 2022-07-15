d9de63a8641e20d830270c3d4069dc525ef330c4
xesam <xesamguo@gmail.com>
2022-07-15 13:39:24 +0800

    0.0.4 /fix

--- pages/hex/index.js
+++ pages/hex/index.js
@@ -8,23 +8,24 @@ Page({
     },
     onLoad(query) {
         const cellsStyle = 'display: flex; font-size: 1.5em; background:white; border-bottom: 1px solid #e4e4e4;padding: 0.5em;';
-        const cellStyle = 'flex: 1; text-align: left; color: #666;';
+        const leadingCellStyle = 'flex: 1; text-align: left; color: #666;';
+        const cellStyle = 'flex: 2; text-align: left; color: #666;';
         const sample1 = new Array(16).fill(0).map((ele, index) => {
             return [index, toHex(index), toBin(index)];
         }).map(ele => {
             return `<div style="${cellsStyle}">
-<span style="${cellStyle}">${ele[0]}</span>
+<span style="${leadingCellStyle}">${ele[0]}</span>
 <span style="${cellStyle}">${ele[1]}</span>
 <span style="${cellStyle}">${ele[2]}</span>
 </div>`;
         });
 
-        const sample2 = new Array(17).fill(0).map((ele, index) => {
+        const sample2 = new Array(33).fill(0).map((ele, index) => {
             const pow = Math.pow(2, index);
             return [index, pow, toHex(pow)];
         }).map(ele => {
             return `<div style="${cellsStyle}">
-<span style="${cellStyle}">2<sup>${ele[0]}</sup></span>
+<span style="${leadingCellStyle}">2<sup>${ele[0]}</sup></span>
 <span style="${cellStyle}">${ele[1]}</span>
 <span style="${cellStyle}">${ele[2]}</span>
 </div>`;
